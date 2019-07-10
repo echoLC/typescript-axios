ecec0cd8dc25d863107236999a02f3e27ca0cd79
echoLC <651644100@qq.com>
2019-07-10 22:56:11 +0800

    feat: 添加了处理get请求params的功能

--- examples/server.js
+++ examples/server.js
@@ -31,6 +31,10 @@ router.get('/simple/get', (req, res) => {
   })
 })
 
+router.get('/base/get', (req, res) => {
+  res.json(req.query)
+})
+
 app.use(router)
 
 const port = process.env.PORT || 9000
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { axiosRequestConfig } from './types'
+import { buildURL } from './helpers/url'
 import xhr from './xhr'
 
 function axios (config: axiosRequestConfig):void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig (config: axiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL (config: axiosRequestConfig): string {
+  const { url, params } = config
+
+  return buildURL(url, params)
 }
 
 export default axios
