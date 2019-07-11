5aa76a2eeec68cfb1cf3bb4d0acb5e483be3ae4c
echoLC <651644100@qq.com>
2019-07-12 00:35:54 +0800

    feat: 完成了axios的处理post data，request header，response data等功能

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,62 +1,88 @@
 import axios from '../../src/index'
 
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['baz', 'bar']
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date: new Date()
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     baz: 'bar'
+//   }
+// })
+
 axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['baz', 'bar']
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 3,
+    b: 4
+  },
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
   }
+}).then((res) => {
+  console.log(res)
 })
 
 axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
+  method: 'post',
+  url: '/base/post',
+  responseType: 'json',
+  data: {
+    a: 1,
+    b: 2
   }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date: new Date()
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    baz: 'bar'
-  }
+}).then((res) => {
+  console.log(res)
 })
 
 
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,10 +1,45 @@
-import { axiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
-export default function xhr (config: axiosRequestConfig):void {
-  const { data = null, url, method = 'get' } = config
+export default function xhr (config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve) => {
+    const { data = null, url, method = 'get', headers, responseType } = config
   
-  const request = new XMLHttpRequest()
-  request.open(method.toUpperCase(), url, true)
+    const request = new XMLHttpRequest()
 
-  request.send(data)
+    if (responseType) {
+      request.responseType = responseType
+    }
+
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleHXRLoad () {
+      if (request.readyState !== 4) {
+        return 
+      }
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType === 'text' ? request.responseText : request.response
+
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+
+      resolve(response)
+    }
+  
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      }
+      request.setRequestHeader(name, headers[name])
+    })
+  
+    request.send(data)
+  })
 }
