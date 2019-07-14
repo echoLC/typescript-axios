2dee96752589130a006f8ba67bd8d6474e9b0f8b
echoLC <651644100@qq.com>
2019-07-14 22:41:26 +0800

    feat: 扩展了axios get, post, delete等一系列api

--- src/axios.ts
+++ src/axios.ts
@@ -1,41 +1,16 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-import xhr from './xhr'
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/utils'
 
-function axios (config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  
-  return xhr(config).then((res) => {
-    res.data = transformResponseData(res)
-    return res
-  })
+function createInstance (): AxiosInstance {
+  const context = new Axios()
+  const instance = Axios.prototype.request.bind(context)
+
+  extend(instance, context)
+
+  return instance as AxiosInstance
 }
 
-function processConfig (config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformURL (config: AxiosRequestConfig): string {
-  const { url, params } = config
-
-  return buildURL(url, params)
-}
-
-function transformRequestData (config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformResponseData (res: AxiosResponse): any {
-  return transformResponse(res.data)
-}
-
-function transformHeaders (config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
+const axios = createInstance()
 
 export default axios
