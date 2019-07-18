57972238040ba62c6bb2081e161fadbe5f60727a
echoLC <651644100@qq.com>
2019-07-18 14:24:30 +0800

    feat: axios增加了create方法

--- src/axios.ts
+++ src/axios.ts
@@ -1,17 +1,22 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/utils'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance (initCnfig: AxiosRequestConfig): AxiosInstance {
+function createInstance (initCnfig: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(initCnfig)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create (config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
