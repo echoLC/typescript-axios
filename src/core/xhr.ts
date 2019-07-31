721f1c56fdc5e4bccc15bbacc27b10aa30f23f71
echoLC <651644100@qq.com>
2019-07-31 22:09:16 +0800

    feat: 添加了支持监听下载文件进度和上传文件进度的功能

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -23,78 +23,107 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       timeout, 
       withCredentials,
       xsrfCookieName,
-      xsrfHeaderName
+      xsrfHeaderName,
+      onDownloadProgress,
+      onUploadProgress
     } = config
 
     const request = new XMLHttpRequest()
 
-    if (responseType) {
-      request.responseType = responseType
-    }
-
-    if (timeout) {
-      request.timeout = timeout
-    }
-
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
-
     request.open(method.toUpperCase(), url!, true)
 
-    request.onreadystatechange = function handleHXRLoad() {
-      const isRequestPending = request.readyState !== 4 || request.status === 0
-      if (isRequestPending) {
-        return
+    // request属性配置
+    configureRequest()
+
+    // request事件监听
+    onRequestEvents()
+
+    // request添加headers
+    processRequestHeaders()
+
+    // 取消request
+    requestCancel()
+
+    request.send(data)
+
+    function configureRequest (): void {
+      if (responseType) {
+        request.responseType = responseType
       }
-
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData = responseType === 'text' ? request.responseText : request.response
-
-      const response: AxiosResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
+  
+      if (timeout) {
+        request.timeout = timeout
       }
-
-      handleResonpse(response)
-    }
-
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
-    }
-
-    request.ontimeout = function handleTimeout() {
-      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED',
-        request))
-    }
-
-    debugger
-    if ((withCredentials && isURLSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfValue = cookie.read(xsrfCookieName)
-      if (xsrfValue && xsrfHeaderName) {
-        request.setRequestHeader(xsrfHeaderName, xsrfValue)
+  
+      if (withCredentials) {
+        request.withCredentials = withCredentials
       }
     }
 
-    Object.keys(headers).forEach(name => {
-      if (data === null && name.toLowerCase() === 'content-type') {
-        delete headers[name]
+    function onRequestEvents (): void {
+      request.onreadystatechange = function handleHXRLoad() {
+        const isRequestPending = request.readyState !== 4 || request.status === 0
+        if (isRequestPending) {
+          return
+        }
+  
+        const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+        const responseData = responseType === 'text' ? request.responseText : request.response
+  
+        const response: AxiosResponse = {
+          data: responseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: responseHeaders,
+          config,
+          request
+        }
+  
+        handleResonpse(response)
       }
-      request.setRequestHeader(name, headers[name])
-    })
+  
+      request.onerror = function handleError() {
+        reject(createError('Network Error', config, null, request))
+      }
+  
+      request.ontimeout = function handleTimeout() {
+        reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED',
+          request))
+      }
+  
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+  
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
+      }
+    }
 
-    if (config.cancelToken) {
-      config.cancelToken.promise.then(reason => {
-        request.abort()
-        reject(reason)
+    function processRequestHeaders (): void {
+      if ((withCredentials && isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+        if (xsrfValue && xsrfHeaderName) {
+          request.setRequestHeader(xsrfHeaderName, xsrfValue)
+        }
+      }
+  
+      Object.keys(headers).forEach(name => {
+        if (data === null && name.toLowerCase() === 'content-type') {
+          delete headers[name]
+        }
+        request.setRequestHeader(name, headers[name])
       })
     }
 
-    request.send(data)
+    function requestCancel (): void {
+      if (config.cancelToken) {
+        config.cancelToken.promise.then(reason => {
+          request.abort()
+          reject(reason)
+        })
+      }
+    }
 
     function handleResonpse(response: AxiosResponse): void {
       if (response.status >= 200 && response.status < 300) {
