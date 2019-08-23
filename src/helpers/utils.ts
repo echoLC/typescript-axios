81308076b1df769b85131a00a2eab171049c4874
echoLC <651644100@qq.com>
2019-08-23 23:00:12 +0800

    feat: 添加了helper下的utils的测试

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,29 +1,29 @@
 const toString = Object.prototype.toString
 
-export function isDate (val: any): val is Date {
-  return toString.call(val) === '[Object Date]'
+export function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
 }
 
-export function isPlainObject (val: any): val is Object {
+export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
 
-export function isFormData (val: any): val is FormData {
+export function isFormData(val: any): val is FormData {
   return typeof val !== undefined && val instanceof FormData
 }
 
-export function isURLSearchParams (val: any): val is URLSearchParams {
+export function isURLSearchParams(val: any): val is URLSearchParams {
   return typeof val !== undefined && val instanceof URLSearchParams
 }
 
-export function extend<T, U> (to: T, from: U): T & U {
+export function extend<T, U>(to: T, from: U): T & U {
   for (const key in from) {
     ;(to as T & U)[key] = from[key] as any
   }
   return to as T & U
 }
 
-export function deepMerge (...objs: any[]): any {
+export function deepMerge(...objs: any[]): any {
   const result = Object.create(null)
 
   objs.forEach(obj => {
